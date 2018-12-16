f6bfe4e26e06cb90cbd46c7b56a50630e887a377
Alex <alexander.orlov@loxal.net>
2018-12-16 23:22:04 +0100

    fix b/g deployment

--- switch-release.sh
+++ switch-release.sh
@@ -16,25 +16,24 @@ isBlueUp() {
 }
 
 runService() {
-echo ssh ubuntu@main.sitesearch.cloud docker run -d --name $1 --log-driver=gelf --log-opt gelf-address=udp://logs.sitesearch.cloud:12201 --env SIS_API_SERVICE_URL=$SIS_API_SERVICE_URL --env SERVICE_SECRET=$SERVICE_SECRET --env SIS_SERVICE_HOST=$SIS_SERVICE_HOST --env WOO_COMMERCE_CONSUMER_KEY=${WOO_COMMERCE_CONSUMER_KEY} --env WOO_COMMERCE_CONSUMER_SECRET=${WOO_COMMERCE_CONSUMER_SECRET} --env BASIC_ENCODED_PASSWORD="$BASIC_ENCODED_PASSWORD" --env ADMIN_SITE_SECRET=$ADMIN_SITE_SECRET --env INVISIBLE_RECAPTCHA_SITE_SECRET=${INVISIBLE_RECAPTCHA_SITE_SECRET} --env SPRING_SECURTY_USER_PASSWORD=$SPRING_SECURITY_USER_PASSWORD --env BUILD_NUMBER=$BUILD_NUMBER --env SCM_HASH=$SCM_HASH --env SECURITY_OAUTH2_CLIENT_CLIENT_SECRET=$SECURITY_OAUTH2_CLIENT_CLIENT_SECRET --restart unless-stopped --network sitesearch docker-registry.sitesearch.cloud/intrafind/if-sitesearch:latest
-#    ssh ubuntu@main.sitesearch.cloud docker run -d --name $1 \
-#        --log-driver=gelf \
-#        --log-opt gelf-address=udp://logs.sitesearch.cloud:12201 \
-#        --env SIS_API_SERVICE_URL=$SIS_API_SERVICE_URL \
-#        --env SERVICE_SECRET=$SERVICE_SECRET \
-#        --env SIS_SERVICE_HOST=$SIS_SERVICE_HOST \
-#        --env WOO_COMMERCE_CONSUMER_KEY="$WOO_COMMERCE_CONSUMER_KEY" \
-#        --env WOO_COMMERCE_CONSUMER_SECRET="$WOO_COMMERCE_CONSUMER_SECRET" \
-#        --env BASIC_ENCODED_PASSWORD="$BASIC_ENCODED_PASSWORD" \
-#        --env ADMIN_SITE_SECRET=$ADMIN_SITE_SECRET \
-#        --env INVISIBLE_RECAPTCHA_SITE_SECRET=${INVISIBLE_RECAPTCHA_SITE_SECRET} \
-#        --env SPRING_SECURTY_USER_PASSWORD=$SPRING_SECURITY_USER_PASSWORD \
-#        --env BUILD_NUMBER=$BUILD_NUMBER \
-#        --env SCM_HASH=$SCM_HASH \
-#        --env SECURITY_OAUTH2_CLIENT_CLIENT_SECRET=$SECURITY_OAUTH2_CLIENT_CLIENT_SECRET \
-#        --restart unless-stopped \
-#        --network $docker_network \
-#        ${img_fqn}
+echo ssh ubuntu@main.sitesearch.cloud docker run -d --name $1 --log-driver=gelf --log-opt gelf-address=udp://logs.sitesearch.cloud:12201 --env SIS_API_SERVICE_URL=$SIS_API_SERVICE_URL --env SERVICE_SECRET=$SERVICE_SECRET --env SIS_SERVICE_HOST=$SIS_SERVICE_HOST --env WOO_COMMERCE_CONSUMER_KEY=${WOO_COMMERCE_CONSUMER_KEY} --env WOO_COMMERCE_CONSUMER_SECRET=${WOO_COMMERCE_CONSUMER_SECRET} --env ADMIN_SITE_SECRET=$ADMIN_SITE_SECRET --env INVISIBLE_RECAPTCHA_SITE_SECRET=${INVISIBLE_RECAPTCHA_SITE_SECRET} --env SPRING_SECURTY_USER_PASSWORD=$SPRING_SECURITY_USER_PASSWORD --env BUILD_NUMBER=$BUILD_NUMBER --env SCM_HASH=$SCM_HASH --env SECURITY_OAUTH2_CLIENT_CLIENT_SECRET=$SECURITY_OAUTH2_CLIENT_CLIENT_SECRET --restart unless-stopped --network sitesearch docker-registry.sitesearch.cloud/intrafind/if-sitesearch:latest
+echo    ssh ubuntu@main.sitesearch.cloud docker run -d --name $1 \
+        --log-driver=gelf \
+        --log-opt gelf-address=udp://logs.sitesearch.cloud:12201 \
+        --env SIS_API_SERVICE_URL=$SIS_API_SERVICE_URL \
+        --env SERVICE_SECRET=$SERVICE_SECRET \
+        --env SIS_SERVICE_HOST=$SIS_SERVICE_HOST \
+        --env WOO_COMMERCE_CONSUMER_KEY="$WOO_COMMERCE_CONSUMER_KEY" \
+        --env WOO_COMMERCE_CONSUMER_SECRET="$WOO_COMMERCE_CONSUMER_SECRET" \
+        --env ADMIN_SITE_SECRET=$ADMIN_SITE_SECRET \
+        --env INVISIBLE_RECAPTCHA_SITE_SECRET=${INVISIBLE_RECAPTCHA_SITE_SECRET} \
+        --env SPRING_SECURTY_USER_PASSWORD=$SPRING_SECURITY_USER_PASSWORD \
+        --env BUILD_NUMBER=$BUILD_NUMBER \
+        --env SCM_HASH=$SCM_HASH \
+        --env SECURITY_OAUTH2_CLIENT_CLIENT_SECRET=$SECURITY_OAUTH2_CLIENT_CLIENT_SECRET \
+        --restart unless-stopped \
+        --network $docker_network \
+        ${img_fqn}
 }
 
 startComponent() {
