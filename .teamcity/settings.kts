f82706848852a474af4ae95ba0dd6413ecdf5616
Alex <alexander.orlov@loxal.net>
2019-01-11 14:09:30 +0100

    cleanup TC DSL

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -206,6 +206,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright 2019 IntraFind Software AG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.sshAgent
@@ -251,11 +267,9 @@ project {
     vcsRoot(HttpsGithubComLoxalIfSitesearchRefsHeadsMaster1)
     vcsRoot(Daily)
     vcsRoot(SmokeApiHealthChecksIfSitesearch)
-//    vcsRoot(IFinderCore)
 
     buildType(Recrawl)
     buildType(LoadTest)
-//    buildType(UpdateIFinderCore)
     buildType(DailySnapshotsAndCleanups)
     buildType(BGRelease)
     buildType(Build)
@@ -607,9 +621,6 @@ object ReleaseRouter : BuildType({
     steps {
         script {
             scriptContent = """
-                sh release-router.sh
-                sh release-router.sh
-
                 #ssh alexander_orlov@dev "cd ${'$'}PWD; sh ./release-router.sh"
             """.trimIndent()
         }
@@ -639,7 +650,7 @@ object SmokeTest : BuildType({
     steps {
         script {
             scriptContent = "sh ./ci/smoke-test.sh"
-            dockerImage = "openjdk:12-alpine"
+            dockerImage = "openjdk:12-jdk-alpine"
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
             dockerRunParameters = "-v /root/.gradle:/root/.gradle"
         }
