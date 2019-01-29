c8301c82462c4be5da18563c3cd6a936e1f9dcde
Alex <alexander.orlov@loxal.net>
2019-01-29 21:50:14 +0100

    cleanup

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+package _Self.buildTypes
+
 /*
 The settings script is an entry point for defining a TeamCity
 project hierarchy. The script should contain a single call to the
