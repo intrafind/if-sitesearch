5a10ace771a5a719f1d0606e65aa3c6b64ecef02
Alex <alexander.orlov@loxal.net>
2019-02-11 13:00:29 +0100

    cleanup

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -62,6 +62,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright 2019 IntraFind Software AG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 val springBootVersion1 = "2.1.2.RELEASE"
 // Migrate to Kotlin https://guides.gradle.org/migrating-build-logic-from-groovy-to-kotlin/
 plugins {
@@ -87,10 +103,6 @@ dependencies {
     val swaggerVersion = "2.9.2"
     val tikaVersion = "1.19.1"
 
-//    configurations.compile.exclude(module = "spring-boot-starter-tomcat")
-//    configurations.compile.exclude(group = "org.springframework.boot", module = "undertow-websockets-jsr")
-//    configurations.compile.exclude(module = "undertow-websockets-jsr")
-
     runtimeOnly("org.apache.tika:tika-parsers:$tikaVersion")
     compile("org.apache.tika:tika:$tikaVersion")
     compile("org.springframework.security.oauth.boot:spring-security-oauth2-autoconfigure:$springBootVersion")
@@ -115,12 +127,11 @@ dependencies {
     testCompile("org.openjdk.jmh:jmh-core:1.21")
     testCompile("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
 
-    compile("org.codehaus.groovy:groovy-templates:2.5.5")
-//    compile("org.springframework.boot:spring-boot-starter-groovy-templates:$springBootVersion")
-    testRuntimeOnly("org.springframework.boot:spring-boot-devtools:$springBootVersion")
-//    compile("org.springframework.boot:spring-boot-devtools:$springBootVersion")
+    compile("org.codehaus.groovy:groovy-templates:2.5.6")
+//    testRuntimeOnly("org.springframework.boot:spring-boot-devtools:$springBootVersion")
+    compile("org.springframework.boot:spring-boot-devtools:$springBootVersion")
 
-    compile("com.squareup.okhttp3:okhttp:3.12.1")
+    compile("com.squareup.okhttp3:okhttp:3.13.1")
     compile("org.jsoup:jsoup:1.11.3")
 
     compile("org.mnode.mstor:mstor:1.0.0")
