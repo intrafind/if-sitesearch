98f20e2b6a16fedf7137fce82d7527f8b9b71fd8
Alex <alexander.orlov@loxal.net>
2019-02-13 22:26:28 +0100

    cleanup

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -30,6 +30,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright 2019 IntraFind Software AG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 val springBootVersion1 = "2.1.2.RELEASE"
 // Migrate to Kotlin https://guides.gradle.org/migrating-build-logic-from-groovy-to-kotlin/
 plugins {
@@ -39,7 +55,7 @@ plugins {
     id("org.springframework.boot") version "2.1.2.RELEASE"
     id("io.spring.dependency-management") version "1.0.6.RELEASE"
     id("io.morethan.jmhreport") version "0.9.0"
-    id("com.google.cloud.tools.jib") version "1.0.0"
+//    id("com.google.cloud.tools.jib") version "1.0.0"
 }
 
 java {
@@ -93,12 +109,11 @@ dependencies {
     compile("com.google.apis:google-api-services-gmail:v1-rev20181202-1.27.0")
 }
 
-jib {
-    from.image = "openjdk:12-jdk-alpine"
-    to.image = "docker-registry.intrafind.net/intrafind/sis-sitesearch:latest"
-    container.mainClass = "com.intrafind.sitesearch.Application"
-//    container.appRoot = "/srv"
-}
+//jib {
+//    from.image = "openjdk:12-jdk-alpine"
+//    to.image = "docker-registry.intrafind.net/intrafind/sis-sitesearch:latest"
+//    container.mainClass = "com.intrafind.sitesearch.Application"
+//}
 
 jmh {
     include = listOf("LoadTest")
