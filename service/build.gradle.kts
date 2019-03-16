25ccc71651ec3757fd089e09e72823cfbb025309
Alex <alexander.orlov@loxal.net>
2019-03-16 20:37:28 +0100

    update http client

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -46,6 +46,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright 2019 IntraFind Software AG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Migrate to Kotlin https://guides.gradle.org/migrating-build-logic-from-groovy-to-kotlin/
 plugins {
     java
@@ -104,7 +120,7 @@ dependencies {
     compile("org.codehaus.groovy:groovy-templates:2.5.6")
     runtimeClasspath("org.springframework.boot:spring-boot-devtools:$springBootVersion")
 
-    compile("com.squareup.okhttp3:okhttp:3.13.1")
+    compile("com.squareup.okhttp3:okhttp:3.14.0")
     compile("org.jsoup:jsoup:1.11.3")
 
     compile("org.mnode.mstor:mstor:1.0.0")
