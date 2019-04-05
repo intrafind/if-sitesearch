d13e7d0e935db12e5d8c53bf93f4abce7c898fbc
Alex <alexander.orlov@loxal.net>
2019-04-05 17:15:32 +0200

    update libraries

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -30,13 +30,29 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright 2019 IntraFind Software AG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Migrate to Kotlin https://guides.gradle.org/migrating-build-logic-from-groovy-to-kotlin/
 plugins {
     java
     idea
     id("me.champeau.gradle.jmh") version "0.4.8"
     id("io.morethan.jmhreport") version "0.9.0"
-    id("org.springframework.boot") version "2.1.3.RELEASE"
+    id("org.springframework.boot") version "2.1.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.7.RELEASE"
     id("com.google.cloud.tools.jib") version "1.0.0"
 }
@@ -57,7 +73,7 @@ repositories {
 }
 
 dependencies {
-    val springBootVersion = "2.1.3.RELEASE"
+    val springBootVersion = "2.1.4.RELEASE"
     val swaggerVersion = "2.9.2"
     val tikaVersion = "1.19.1"
 
@@ -67,7 +83,7 @@ dependencies {
     compile("org.springframework.boot:spring-boot-starter-security:$springBootVersion")
 
     compile("edu.uci.ics:crawler4j:4.4.0")
-    compile("com.github.crawler-commons:crawler-commons:0.10")
+    compile("com.github.crawler-commons:crawler-commons:1.0")
 
     compile("com.rometools:rome:1.12.0")
 
