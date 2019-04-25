9d55f0e4421d8c2bb5b42ba7b10cb5e73e5513b8
bachka <ichinnorovb@gmail.com>
2019-04-26 00:03:46 +0200

    clean up bcs bundle all ssl cert issue to al-api: //al-api/ops/cerbot-ssl-cert/

---
+++ run-sis-router.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+docker_network=sitesearch
+docker_redirect_image=router
+docker_tag=latest
+
+cd docker-${docker_redirect_image}
+docker pull docker-registry.sitesearch.cloud/intrafind/${docker_redirect_image}:${docker_tag} .
+docker rm -f $docker_redirect_image
+docker run -d --name $docker_redirect_image \
+    -p 80:80 \
+    -p 443:443 \
+    -v /etc/letsencrypt:/etc/letsencrypt \
+    --restart unless-stopped \
+    --network $docker_network \
+    docker-registry.sitesearch.cloud/intrafind/${docker_redirect_image}:${docker_tag}
