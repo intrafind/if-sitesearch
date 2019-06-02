a355a62fd37db28d590c4bbee2f4dc2ddfd5ed68
Alex <alexander.orlov@loxal.net>
2019-06-02 13:11:57 +0200

    add additional Basic Auth password to router

--- init-oss.sh
+++ init-oss.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-#docker exec -t ops-es curl -X PUT \
 docker exec -t elk-elasticsearch curl -X PUT \
+docker exec -t ops-es curl -X PUT \
   http://localhost:9200/site-profile/_doc/site-configuration-b7fde685-33f4-4a79-9ac3-ee3b75b83fa3 \
   -H 'Content-Type: application/json' \
   -d '{
