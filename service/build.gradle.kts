03bb7c6bddf54b94b3281437832014ff9879d42f
Alex <alexander.orlov@loxal.net>
2019-06-08 13:25:28 +0200

    update libraries

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -30,6 +30,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright 2019 IntraFind Software AG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Migrate to Kotlin https://guides.gradle.org/migrating-build-logic-from-groovy-to-kotlin/
 plugins {
     java
@@ -38,7 +54,7 @@ plugins {
     id("io.morethan.jmhreport") version "0.9.0"
     id("org.springframework.boot") version "2.1.5.RELEASE"
     id("io.spring.dependency-management") version "1.0.7.RELEASE"
-    id("com.google.cloud.tools.jib") version "1.2.0"
+    id("com.google.cloud.tools.jib") version "1.3.0"
 }
 
 idea {
@@ -92,8 +108,8 @@ dependencies {
     compile("org.jsoup:jsoup:1.12.1")
 
     compile("org.mnode.mstor:mstor:1.0.0")
-    compile("com.google.oauth-client:google-oauth-client-jetty:1.28.0")
-    compile("com.google.apis:google-api-services-gmail:v1-rev103-1.23.0")
+    compile("com.google.oauth-client:google-oauth-client-jetty:1.30.1")
+    compile("com.google.apis:google-api-services-gmail:v1-rev104-1.25.0")
 }
 
 jib {
