7909aaf7a493a91ad3eb49c1ed102527a1611867
Alex <alexander.orlov@loxal.net>
2019-06-27 09:51:39 +0200

    update okhttp

--- js-app-build.sh
+++ js-app-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-./gradlew :payment:build :finder:build --parallel --continuous --build-cache $1
+./gradlew :payment:build :finder:build :dashboard:build --parallel --continuous --build-cache $1
 #./gradlew :gadget:build :dashboard:build --parallel --continuous --build-cache $1
---
+++ run-oss.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# add "--debug-jvm" to attach debugger
+{
+    SPRING_PROFILES_ACTIVE=oss \
+    SPRING_CONFIG_NAME="application, local" \
+        ./gradlew bootRun --continue --no-scan --no-build-cache $1
+#        ./gradlew bootRun --continue --continuous --no-scan --parallel --build-cache --refresh-dependencies $1
+#        --refresh-dependencies --rerun-tasks --no-build-cache $1
+} || {
+    hangingJavaProcessToStop=`jps | grep Application | awk '{print $1}'`
+    echo "hangingJavaProcessToStop: $hangingJavaProcessToStop"
+    kill -9 $hangingJavaProcessToStop
+    echo "Gracefully killed hanging process: $hangingJavaProcessToStop"
+}
+
+#touch service/build/resources/main/application.yaml # to trigger Spring Boot reload
+#./gradlew :service:compileJava :service:processResources # to trigger reload
