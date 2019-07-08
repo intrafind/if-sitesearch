d1f33d7b90b20ea6d12fbba5b7eabfc9c9a3a87e
Alex <alexander.orlov@loxal.net>
2019-07-08 04:02:02 +0200

    introduce crucial sleep time

--- service/src/test/java/com/intrafind/sitesearch/integration/SiteTest.java
+++ service/src/test/java/com/intrafind/sitesearch/integration/SiteTest.java
@@ -276,7 +276,7 @@ public class SiteTest {
         assertEquals(HttpStatus.NO_CONTENT, deletion.getStatusCode());
         assertNull(deletion.getBody());
 
-        TimeUnit.SECONDS.sleep(25);
+        TimeUnit.SECONDS.sleep(30);
         // fetch via URL an already deleted page
         final var fetchViaUrlForNonExistingPage = caller.exchange(SiteController.ENDPOINT
                         + "/" + newSite.getSiteId() + "/pages?url=" + newPage.getUrl(),
