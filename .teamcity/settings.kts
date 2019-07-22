12a9f0120535d190e4bebde013d19800a9fc4985
Alex <alexander.orlov@loxal.net>
2019-07-22 12:52:37 +0200

    update CI settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 IntraFind Software AG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildFeatures.sshAgent
@@ -202,7 +218,7 @@ object Build : BuildType({
         script {
             name = "Build service.jar w/ Docker (using TeamCity Docker plugin)"
             scriptContent = """
-                #SPRING_PROFILES_ACTIVE=oss ./gradlew clean build --info
+                #SPRING_PROFILES_ACTIVE=oss
                 ./gradlew clean includeKotlinJsRuntime build --info
             """.trimIndent()
             dockerImage = "openjdk:13-alpine"
