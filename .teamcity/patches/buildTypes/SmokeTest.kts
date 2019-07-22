26bbd8ce5a281bd23655ab904a39d44433600186
Alex <alexander.orlov@loxal.net>
2019-07-22 12:36:50 +0200

    update CI settings

--- .teamcity/patches/buildTypes/SmokeTest.kts
+++ .teamcity/patches/buildTypes/SmokeTest.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 IntraFind Software AG. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
@@ -13,8 +29,8 @@ accordingly, and delete the patch script.
 changeBuildType(RelativeId("SmokeTest")) {
     expectSteps {
         script {
-            scriptContent = "sh ./ci/smoke-test.sh"
-            dockerImage = "openjdk:12-jdk-alpine"
+            scriptContent = "sh ./ops/smoke-test.sh"
+            dockerImage = "openjdk:13-alpine"
             dockerImagePlatform = ScriptBuildStep.ImagePlatform.Linux
             dockerRunParameters = "-v /root/.gradle:/root/.gradle"
         }
