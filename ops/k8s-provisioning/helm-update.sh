194fc0634171d6a926832840c8223522d25754cf
Alex <alexander.orlov@loxal.net>
2019-08-31 23:59:17 +0200

    introduce secret delegation

--- ops/k8s-provisioning/helm-update.sh
+++ ops/k8s-provisioning/helm-update.sh
@@ -17,7 +17,7 @@ sleep 13
 #  helm upgrade $helmName /srv/$helmName --namespace $workspace \
 ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node \
   helm install /srv/$helmName --name $helmName --namespace $workspace \
-  --set app.tenant=$workspace,app.HETZNER_API_TOKEN=$TF_VAR_hetzner_cloud_intrafind,app.password=$TF_VAR_password,app.dockerRegistrySecret=$TF_VAR_docker_registry_k8s_secret \
+  --set app.tenant=$workspace,app.HETZNER_API_TOKEN=$TF_VAR_hetzner_cloud_intrafind,app.adminSecret=$ADMIN_SITE_SECRET,app.dockerRegistrySecret=$TF_VAR_docker_registry_k8s_secret,app.sis.wooCommerceConsumerKey=$WOO_COMMERCE_CONSUMER_KEY,app.sis.wooCommerceConsumerSecret=$WOO_COMMERCE_CONSUMER_SECRET,app.sis.serviceSecret=$SERVICE_SECRET,app.meta.scmHash=$SCM_HASH,app.meta.buildNumber=$BUILD_NUMBER,app.recaptchaSecret=$RECAPTCHA_SITE_SECRET \
   --set-string app.volumeHandle=3052845
 
 ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node \
