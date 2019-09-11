854afbdbad3c45c78fc3c742fb9132df9e198016
Alex <alexander.orlov@loxal.net>
2019-09-11 12:19:01 +0200

    cleanup

--- ops/k8s-provisioning/helm-update.sh
+++ ops/k8s-provisioning/helm-update.sh
@@ -14,14 +14,6 @@ scp -q -o StrictHostKeyChecking=no -r asset/$helmName root@$k8s_master_node:/opt
 #ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node helm delete ingress --purge
 #sleep 13
 
-#ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node \
-#  helm install /opt/$helmName --name $helmName --namespace $workspace \
-#  --set app.tenant=$workspace,app.HETZNER_API_TOKEN=$TF_VAR_hetzner_cloud_intrafind,app.adminSecret=$ADMIN_SITE_SECRET, \
-#  --set app.dockerRegistrySecret=$TF_VAR_docker_registry_k8s_secret,app.sis.wooCommerceConsumerKey=$WOO_COMMERCE_CONSUMER_KEY \
-#  --set app.sis.wooCommerceConsumerSecret=$WOO_COMMERCE_CONSUMER_SECRET,app.sis.serviceSecret=$SERVICE_SECRET, \
-#  --set app.meta.scmHash=$SCM_HASH,app.meta.buildNumber=$BUILD_NUMBER,app.recaptchaSecret=$RECAPTCHA_SITE_SECRET, \
-#  --set-string app.volumeHandle=3052845, \
-#  --set app.basicAuth=$BASIC_ENCODED_PASSWORD
 ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node \
   helm upgrade $helmName /opt/$helmName --install --namespace $workspace \
   --set app.tenant=$workspace,app.HETZNER_API_TOKEN=$TF_VAR_hetzner_cloud_intrafind,app.adminSecret=$ADMIN_SITE_SECRET, \
@@ -31,8 +23,6 @@ ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node \
   --set-string app.volumeHandle=3052845, \
   --set app.basicAuth=$BASIC_ENCODED_PASSWORD
 
-#ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node \
-#  helm install --name ingress stable/nginx-ingress --set rbac.create=true,controller.hostNetwork=true,controller.kind=DaemonSet
 ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node \
   helm upgrade ingress stable/nginx-ingress --install --namespace $workspace --set rbac.create=true,controller.hostNetwork=true,controller.kind=DaemonSet
 
