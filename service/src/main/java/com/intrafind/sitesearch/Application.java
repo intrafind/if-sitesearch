c75bcb12e220103012f207b3d3d1853ba5e1f8cc
Alex <alexander.orlov@loxal.net>
2019-09-21 14:42:01 +0200

    protect sis router

--- service/src/main/java/com/intrafind/sitesearch/Application.java
+++ service/src/main/java/com/intrafind/sitesearch/Application.java
@@ -56,7 +56,7 @@ public class Application {
     private static final String WOO_COMMERCE_CONSUMER_SECRET = System.getenv("WOO_COMMERCE_CONSUMER_SECRET");
     private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
     public static final String SIS_DOMAIN = "sitesearch.cloud";
-    public static final String OSS_SIS_DOMAIN = "loxal.net";
+    public static final String OSS_SIS_DOMAIN = "sitesearch.cloud";
     private static Mac macSha1Algorithm;
 
     static {
