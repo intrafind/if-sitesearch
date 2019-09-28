8b936da125981e614ad669796bdd3a0aa88d5efe
Alex <alexander.orlov@loxal.net>
2019-09-28 22:17:47 +0200

    make load tests generic

--- load-test.sh
+++ load-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-SPRING_PROFILES_ACTIVE=$SPRING_PROFILES_ACTIVE \
+#SPRING_PROFILES_ACTIVE=$SPRING_PROFILES_ACTIVE \
 ./gradlew :service:clean jmh
 #./gradlew runJMH $1
 #./gradlew clean runJMH $1 > /dev/null
