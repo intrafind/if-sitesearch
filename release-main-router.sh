78568f5e84695f4fe90f09137c498abc5eba2791
bachka <bachka@intrafind.de>
2020-02-27 20:32:23 +0100

    prepare sis and main router for release all router

--- release-main-router.sh
+++ release-main-router.sh
@@ -4,6 +4,11 @@ docker_network=main
 docker_redirect_image=main-router
 docker_tag=latest
 
+if [ "$*" != "" ]; then
+    docker_tag="$*"
+    echo "Router image tag is : " $docker_tag
+fi
+
 docker network create $docker_network
 cd main-router
 sudo rm -rf letsencrypt
