55b2cbee5cdec2c52f99d56c02a5d88e628e6d64
bachka <bachka@intrafind.de>
2020-07-22 14:32:42 +0200

    remove old maven mount point in main-router

--- run-main-router.sh
+++ run-main-router.sh
@@ -4,14 +4,12 @@ docker_network=main
 docker_redirect_image=main-router
 docker_tag=latest
 
-chmod -R 777 /mnt/maven/repository
 chmod -R o+x /mnt/ca-on-premise
 docker pull docker-registry.intrafind.net/intrafind/${docker_redirect_image}:${docker_tag}
 docker rm -f $docker_redirect_image
 docker run -d --name $docker_redirect_image \
     -p 80:80 \
     -p 443:443 \
-    -v /mnt/maven/repository:/srv/maven-repository \
     -v /mnt/ca-on-premise:/srv/download \
     --network $docker_network \
     docker-registry.intrafind.net/intrafind/${docker_redirect_image}:${docker_tag}
