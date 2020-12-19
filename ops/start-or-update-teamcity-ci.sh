f94ce537a56ec979b138fb9dbd25d9529737232d
bachka <bachka@intrafind.de>
2020-12-19 19:01:11 +0100

    update teamcity CI

--- ops/start-or-update-teamcity-ci.sh
+++ ops/start-or-update-teamcity-ci.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 # Should be executed on the host with `~/BuildAgent` directory
 # docker pull jetbrains/teamcity-server:2020.2
-version=latest
+version=2020.2.1
 service_name=teamcity-server
 network=main
 
